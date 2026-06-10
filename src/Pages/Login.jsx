f5e49432b1cdd74150be931b28d2db67e40de6e7
Bright-CodeHub <brightcodehub55@gmail.com>
2026-06-10 12:30:09 +0530

    create a login and register page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,22 @@
-import { Container, Paper, TextField, Typography } from '@mui/material'
+import { Button, Container, Paper, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
 const Login = () => {
 
     const [isLogin, setisLogin] = useState(true)
 
+    const toggleLogin = () => setisLogin(prev => !prev)
+
     return (
-        <Container component={'main'} maxWidth='sx'>
+        <Container
+            component={'main'}
+            maxWidth='xs'
+            sx={{
+                height: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }} >
             <Paper
                 sx={{
                     padding: 4,
@@ -22,7 +32,11 @@ const Login = () => {
                     ?
                     <>
                         <Typography variant='h5' >Login</Typography>
-                        <form >
+
+                        <form style={{
+                            width: '100%',
+                            marginTop: '1rem'
+                        }} >
 
                             <TextField
                                 required
@@ -32,10 +46,119 @@ const Login = () => {
                                 variant='outlined'
                                 size='small'
                             />
+                            <TextField
+                                required
+                                fullWidth
+                                label='Password'
+                                margin='normal'
+                                variant='outlined'
+                                size='small'
+                            />
+
+                            <Button
+                                variant='contained'
+                                color='primary'
+                                type='submit'
+                                sx={{
+                                    marginTop: '1rem'
+                                }}
+                                fullWidth
+                            >
+                                Login
+                            </Button>
+
+                            <Typography sx={{
+                                mt: '1rem',
+                                width: '100%',
+                                textAlign: 'center',
+                            }}
+                            >OR</Typography>
+
+                            <Button
+                                variant='text'
+                                color='primary'
+                                type='submit'
+                                fullWidth
+                                onClick={toggleLogin}
+                            >
+                                Sign
+                            </Button>
 
                         </form>
                     </> :
-                    <span>register</span>}
+                    <>
+                        <Typography variant='h5' >Sign up</Typography>
+
+                        <form style={{
+                            width: '100%',
+                            marginTop: '1rem'
+                        }} >
+
+                            <TextField
+                                required
+                                fullWidth
+                                label='Name'
+                                margin='normal'
+                                variant='outlined'
+                                size='small'
+                            />
+                            <TextField
+                                required
+                                fullWidth
+                                label='Bio'
+                                margin='normal'
+                                variant='outlined'
+                                size='small'
+                            />
+                            <TextField
+                                required
+                                fullWidth
+                                label='Username'
+                                margin='normal'
+                                variant='outlined'
+                                size='small'
+                            />
+                            <TextField
+                                required
+                                fullWidth
+                                label='Password'
+                                margin='normal'
+                                variant='outlined'
+                                size='small'
+                            />
+
+                            <Button
+                                variant='contained'
+                                color='primary'
+                                type='submit'
+                                sx={{
+                                    marginTop: '1rem'
+                                }}
+                                fullWidth
+                            >
+                                Sign up
+                            </Button>
+
+                            <Typography sx={{
+                                mt: '1rem',
+                                width: '100%',
+                                textAlign: 'center',
+                            }}
+                            >OR</Typography>
+
+                            <Button
+                                variant='text'
+                                color='primary'
+                                type='submit'
+                                fullWidth
+                                onClick={toggleLogin}
+                            >
+                                Login
+                            </Button>
+
+                        </form>
+                    </>
+                }
 
             </Paper>
 
