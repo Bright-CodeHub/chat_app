ba8f40458e088a682ba5d391fe4454cff46fce03
Bright-CodeHub <brightcodehub55@gmail.com>
2026-06-20 16:05:49 +0530

    progress on home page

--- src/Components/layout/AppLayout.jsx
+++ src/Components/layout/AppLayout.jsx
@@ -12,19 +12,17 @@ const AppLayout = (WrappedComponent) => {
 
                 <Grid container sx={{ height: 'calc(100vh - 4rem)' }} >
 
-                    <Grid size={4} sx={{ bgcolor: 'rgba(0,0,0,0.50)', height: '100%' }} >
+                    <Grid size={{ xs: 'grow' }} sx={{ bgcolor: 'rgba(0,0,0,0.50)', height: '100%', display: {} }} >
                         First
                     </Grid>
 
-                    <Grid size={4} sx={{ bgcolor: 'rgba(0,0,0,0.50)', height: '100%' }} >
+                    <Grid size={{ sm: 'grow' }} sx={{ bgcolor: 'rgba(0,0,0,0.50)', height: '100%', display: { xs: 'none', sm: 'block' } }} >
                         <WrappedComponent />
                     </Grid>
 
-                    <Grid size={4} sx={{ bgcolor: 'rgba(0,0,0,0.50)', height: '100%', display: { sx:'none' , sm:'block' } }} >
+                    <Grid size={4} sx={{ bgcolor: 'rgba(0,0,0,0.50)', height: '100%', display: { xs: 'none', md: 'block' } }} >
                         First
                     </Grid>
-
-
 
                 </Grid >
 
--- src/Components/layout/Header.jsx
+++ src/Components/layout/Header.jsx
@@ -1,22 +1,56 @@
-import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, IconButton, Toolbar, Tooltip, Typography } from '@mui/material'
 import React from 'react'
 import { orange } from '../constants/color'
+import { Add as AddIcon, Menu as MenuIcon, Search as SearchIcon } from '@mui/icons-material'
 
 const Header = () => {
+
+  const handleMobileView = () => {
+
+  }
+
+  const openSearchDialog = () => {
+
+  }
+
+  const openNewGroup = () => {
+
+  }
+
   return (
-    <Box sx={{ height: '4rem', flexGrow: 1 }} >
+    <Box sx={{ height: '4rem' }} >
+
       <AppBar sx={{ position: 'static', bgcolor: orange }} >
 
-
-        <Toolbar>
+        <Toolbar sx={{ minHeight: '64px' }} >
 
           <Typography variant='h6' sx={{ display: { xs: 'none', sm: 'block' } }} >
-
-            Head
+            Chat
           </Typography>
 
-        </Toolbar>
+          <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+            <IconButton color='inherit' onClick={handleMobileView}>
+              <MenuIcon />
+            </IconButton>
+          </Box>
 
+          <Box sx={{ flexGrow: 1 }} >
+
+          </Box>
+
+          <Box>
+            <IconButton size='large' color='inherit' onClick={openSearchDialog} >
+              <SearchIcon />
+            </IconButton>
+
+            <Tooltip title='New Group' >
+              <IconButton size='large' color='inherit' onClick={openNewGroup} >
+                <AddIcon />
+              </IconButton>
+            </Tooltip>
+          </Box>
+
+        </Toolbar>
       </AppBar>
     </Box>
   )
